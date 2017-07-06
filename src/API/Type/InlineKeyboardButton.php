f746795f179fb24e9670075ad3305a7836b1dd6e
Freek van der Veer <freek_vanderveer@hotmail.com>
2017-07-06 23:33:40 +0200

    fixed spacing
    made the file look great again!

--- src/API/Type/InlineKeyboardButton.php
+++ src/API/Type/InlineKeyboardButton.php
@@ -10,13 +10,13 @@ class InlineKeyboardButton extends ABaseObject {
 
     public static function GetDatamodel() : array {
         $datamodel = [
-        'text'                      => ['type' => ABaseObject::T_STRING, 'optional' => FALSE,   'external' => 'text'],
-        'url'                       => ['type' => ABaseObject::T_STRING, 'optional' => TRUE,    'external' => 'url'],
-        'callbackData'              => ['type' => ABaseObject::T_STRING, 'optional' => TRUE,    'external' => 'callback_data'],
-        'switchInlineQuery'         => ['type' => ABaseObject::T_STRING, 'optional' => TRUE,    'external' => 'switch_inline_query'],
-        'switchQueryCurrentChat'    => ['type' => ABaseObject::T_STRING, 'optional' => TRUE,    'external' => 'switch_inline_query_current_chat'],
-        'callbackGame'              => ['type' => ABaseObject::T_OBJECT, 'optional' => TRUE,    'external' => 'callback_game',                      'class' => CallbackGame::class],
-        'pay'                       => ['type' => ABaseObject::T_BOOL,   'optional' => TRUE,    'external' => 'pay']
+        'text'                          => ['type' => ABaseObject::T_STRING, 'optional' => FALSE,   'external' => 'text'],
+        'url'                           => ['type' => ABaseObject::T_STRING, 'optional' => TRUE,    'external' => 'url'],
+        'callbackData'                  => ['type' => ABaseObject::T_STRING, 'optional' => TRUE,    'external' => 'callback_data'],
+        'switchInlineQuery'             => ['type' => ABaseObject::T_STRING, 'optional' => TRUE,    'external' => 'switch_inline_query'],
+        'switchInlineQueryCurrentChat'  => ['type' => ABaseObject::T_STRING, 'optional' => TRUE,    'external' => 'switch_inline_query_current_chat'],
+        'callbackGame'                  => ['type' => ABaseObject::T_OBJECT, 'optional' => TRUE,    'external' => 'callback_game',                      'class' => CallbackGame::class],
+        'pay'                           => ['type' => ABaseObject::T_BOOL,   'optional' => TRUE,    'external' => 'pay']
         ];
         return array_merge(parent::GetDatamodel(), $datamodel);
     }
